6c375d91b0828e5bba217d0642cc9b44baece0ee
Robby Ackerley <rcackerley@me.com>
2018-05-06 16:24:52 -0400

    edited dbconfig var

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const pg = require('pg-promise')();
 // const dbConfig = 'postgres://robby@localhost:5432/brewpub';
-const db = pg(dbConfig);
 const dbConfig = process.env.DATABASE_URL;
+const db = pg(dbConfig);
 
 module.exports = db;
